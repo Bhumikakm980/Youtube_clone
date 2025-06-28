f636029527f69023fd8bdcfc20e60c3e2700b4d1
Bhumika <bhumikaspandana410@gmail.com>
2025-06-29 01:49:50 +0530

    removing details

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-const API_KEY="AIzaSyDiJJPfIRwAbNrmMFOMijZnQnea9veVT44";
-
-export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyBMyzsiU8IzP-xYQzf4cddTXrGAR3nrdsU";
-
-/**api for search suggestion */
-
-export const SEARCH_SUGGESTION_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
